c34a1d8b76150ec4309b9946724f940feb2f2480
Mike Morano <mmorano@mikeandwan.us>
2016-07-22 23:43:20 -0400

    support geting exif data from streams, so the file does not necessarily have to live on disk

--- test/NExifTool.Tests/Test.cs
+++ test/NExifTool.Tests/Test.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Xunit;
 using NExifTool;
@@ -102,6 +103,32 @@ namespace NExifTool.Tests
             Assert.Equal(true, true);
         }
         
+
+        [Fact]
+        public async void TestStream()
+        {
+            using(var ms = new MemoryStream())
+            using(var fs = new FileStream("DSC_3982.NEF", FileMode.Open))
+            {
+                fs.CopyTo(ms);
+                ms.Seek(0, SeekOrigin.Begin);
+
+                var et = new ExifTool(new ExifToolOptions());
+                var tags = (await et.GetTagsAsync(ms)).ToList();
+
+                var bitsPerSample = GetExifData<ushort>(tags, "BitsPerSample")?.TypedValue;
+                var digitalZoomRatio = GetExifData<double>(tags, "DigitalZoomRatio")?.TypedValue;
+                var expTime = GetExifData<double>(tags, "ExposureTime");
+                
+                Assert.True(bitsPerSample == 14, "bits per sample should be 14 for this photo");
+                Assert.True(digitalZoomRatio == 1, "digital zoom ratio should be 1 for this photo");
+                
+                Assert.Equal("1/500", expTime.Value);
+                Assert.Equal("0.002", expTime.NumberValue);
+                Assert.Equal(.002, expTime.TypedValue);
+            }
+        }
+
         
         static Tag<T> GetExifData<T>(IEnumerable<Tag> exifData, string datapoint)
         {
