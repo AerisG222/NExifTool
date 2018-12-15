e5d4cdcf0042f6f9b3959a4ab943ef0713cae88b
Mike Morano <mmorano@mikeandwan.us>
2018-12-15 09:24:20 -0500

    update tools

--- tools/ExifLookupCodegen/Program.cs
+++ tools/ExifLookupCodegen/Program.cs
@@ -10,16 +10,16 @@ namespace NExifTool.Tools
     {
         static string _src;
         static int _count = 1;
-        
-        
+
+
         public static void Main(string[] args)
         {
             _src = args[0];
-            
+
             Generate();
         }
-        
-        
+
+
         public static void Generate()
 		{
             WriteLine("// -------------------------------------------------------------------");
@@ -37,16 +37,16 @@ namespace NExifTool.Tools
             WriteLine("        public static readonly Dictionary<string, TagInfo> Details = new Dictionary<string, TagInfo>();");
             WriteLine();
             WriteLine();
-            
+
             WriteMethods();
-            
+
             WriteLine("        static ExifToolLookup()");
             WriteLine("        {");
-            
+
             WriteInitCalls();
-            
+
             WriteLine("        }");
-            
+
             WriteLine();
             WriteLine();
             WriteLine("        static void AddInfo(TagInfo info)");
@@ -55,50 +55,51 @@ namespace NExifTool.Tools
             WriteLine("            {");
             WriteLine("                Details.Add(info.LookupKey, info);");
             WriteLine("            }");
-            
+
             /* useful for debugging the generator
             WriteLine("            else");
             WriteLine("            {");
             WriteLine("                Console.WriteLine($\"duplicated lookup entry: {info.LookupKey}\");");
             WriteLine("            }");
             */
-            
+
             WriteLine("        }");
 
             WriteLine("    }");
             WriteLine("}");
             WriteLine();
 		}
-        
-        
+
+
         static void WriteMethods()
         {
             var doc = XDocument.Load(_src);
             var tables = doc.Descendants("table");
-            
+
             foreach(var table in tables)
             {
                 WriteLine($"        static void InitTable{_count}()");
                 WriteLine("        {");
-                
+
                 WriteTags(table);
-                
+
                 WriteLine("        }");
                 WriteLine();
                 WriteLine();
-                
+
                 _count++;
             }
         }
-        
-        
+
+
         static void WriteTags(XElement table)
         {
             var tableName = table.Attribute("name").Value;
             var tableG0 = table.Attribute("g0")?.Value;
             var tableG1 = table.Attribute("g1")?.Value;
             var tableG2 = table.Attribute("g2")?.Value;
-            
+
+            // TODO: see if we can do something more useful with index properties that we are ignoring for now
             // for indexed items, only use the first one
             foreach(var tag in table.Descendants("tag").Where(x => x.Attribute("index") == null || string.Equals("0", x.Attribute("index").Value)))
             {
@@ -109,18 +110,18 @@ namespace NExifTool.Tools
                 var g0 = table.Attribute("g0")?.Value ?? tableG0;
                 var g1 = table.Attribute("g1")?.Value ?? tableG1;
                 var g2 = table.Attribute("g2")?.Value ?? tableG2;
-            
+
                 WriteTag(tableName, tagId, tagName, desc, g0, g1, g2, type);
             }
         }
-        
-        
+
+
         static void WriteTag(string tableName, string tagId, string tagName, string desc, string g0, string g1, string g2, string type)
         {
             WriteLine($"            AddInfo(new TagInfo {{ Id = \"{tagId}\", Name = \"{tagName}\", Description = \"{desc}\", TableName = \"{tableName}\", GeneralGroup = \"{g0}\", SpecificGroup = \"{g1}\", CategoryGroup = \"{g2}\", ValueType = \"{type}\" }});");
         }
-        
-                
+
+
         static void WriteInitCalls()
         {
             for(int i = 1; i < _count; i++)
